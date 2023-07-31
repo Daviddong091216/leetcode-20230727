c5571daf10224115603b991fcb9c4950f898cf63
daviddong091216 <daviddong201712@gmail.com>
2023-07-31 09:38:42 -0500

    20230731

---
+++ src/PalindromeNumber_9.java
@@ -0,0 +1,52 @@
+public class PalindromeNumber_9 {
+    public boolean isPalindrome(int x) {
+
+        // If x is a negative number it is not a palindrome
+        // If x % 10 = 0, in order for it to be a palindrome the first digit should also be 0
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        } else if (x == 0) {
+            return true;
+        }
+     /*   int res = 0;
+        while (x > res) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
+        // If x is equal to reversed number then it is a palindrome
+        // If x has odd number of digits, dicard the middle digit before comparing with x
+        // Example, if x = 23132, at the end of for loop x = 23 and reversedNum = 231
+        // So, reversedNum/10 = 23, which is equal to x
+        return (x == res || x == res / 10);
+        */
+
+        long reversedInt = reverse(x);
+        if (x == reversedInt) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static long reverse(long i) {
+        String str = Long.toString(i);
+
+        String tmp = "";
+        for (int j = str.length() - 1; j >= 0; j--) {
+            tmp += str.charAt(j);
+
+        }
+
+        //return Integer.valueOf(tmp);
+        return Long.parseLong(tmp);
+
+    }
+
+    public static void main(String[] args) {
+        int x = 1211;
+        PalindromeNumber_9 obj = new PalindromeNumber_9();
+        System.out.println(obj.isPalindrome(x));
+    }
+}
+
+
--- src/TwoSum_1.java
+++ src/TwoSum_1.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-
-public class TwoSum {
+public class TwoSum_1 {
     public int[] twoSum(int[] nums, int target){
         for(int i =0; i < nums.length; i++){
             for(int j=i+1; j< nums.length; j++){
@@ -12,7 +10,7 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
     public static void main(String[] args) {
-        TwoSum obj= new TwoSum();
+        TwoSum_1 obj= new TwoSum_1();
         int[] nums= {2,7,11,15};
         int target=9;
         obj.twoSum(nums,target);
