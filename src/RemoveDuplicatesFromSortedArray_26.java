779b5671675759499a5b17885072a5587c3a1929
daviddong091216 <daviddong201712@gmail.com>
2023-10-16 13:39:46 -0500

    20231016-2

--- src/RemoveDuplicatesFromSortedArray_26.java
+++ src/RemoveDuplicatesFromSortedArray_26.java
@@ -1,3 +1,24 @@
 public class RemoveDuplicatesFromSortedArray_26 {
-    
+    public int removeDuplicates(int[] nums) {
+
+        // Length of the updated array
+        int count = 0;
+        // Loop for all the elements in the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the current element is equal to the next element, we skip
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            // We will update the array in place
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray_26 obj = new RemoveDuplicatesFromSortedArray_26();
+        
+    }
 }
